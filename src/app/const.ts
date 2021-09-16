a67197017766719830b4b4f358d1ec348fb01319
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-09-16 10:11:08 +0200

    create const.ts

---
+++ src/app/const.ts
@@ -0,0 +1,14 @@
+const defaultAttributes: Object = {
+  min: 0,
+  max: 100,
+  step: 1,
+  from: 10,
+  to: 80,
+  isRange: true,
+  isVertical: false,
+  showTip: true,
+  showBar: true,
+  showScale: false,
+};
+
+export default defaultAttributes;
