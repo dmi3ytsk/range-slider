fbc7874176bfc02a82d1b1ce505d67ee41f35d0e
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-09-17 16:40:38 +0200

    Create interfaces

---
+++ src/app/interfaces/HandleOptions.ts
@@ -0,0 +1,8 @@
+export interface HandleOptions {
+  isVertical: boolean,
+  isRange: boolean,
+  showTip: boolean,
+  currentValue: number,
+  ratio: number,
+}
+
---
+++ src/app/interfaces/TrackOptions.ts
@@ -0,0 +1,9 @@
+export interface TrackOptions {
+  isVertical: boolean,
+  isRange: boolean,
+  showBar: boolean,
+  showTip: boolean,
+  fromCurrentValue: number,
+  toCurrentValue: number,
+  ratios: { fromRatio: number, toRatio: number },
+}
