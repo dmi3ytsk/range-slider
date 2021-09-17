8b0a4b5006a6a617f2c1c0e5ca97f6ecbb7f03b1
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-09-17 16:43:01 +0200

    Use prettier:)

--- src/app/Observer/Observer.ts
+++ src/app/Observer/Observer.ts
@@ -1,18 +1,15 @@
 class Observer {
+  constructor(readonly listeners: any = { listener: [] }) {}
 
-   constructor(readonly listeners: any = { listener: [] }) {
-      
-   }
+  // Подписаться.
+  public subscribe(event: string, callback: Function) {
+    this.listeners[event].data.push(callback);
+  }
 
-	// Подписаться.
-	public subscribe(event: string, callback: Function) {
-		this.listeners[event].data.push(callback);
-	}
-
-	// Разослать сообщение подписчикам.
-	public broadcast(event: string, data: any) {
-		this.listeners[event].data.forEach(listener => listener(data));
-	}
+  // Разослать сообщение подписчикам.
+  public broadcast(event: string, data: any) {
+    this.listeners[event].data.forEach((listener) => listener(data));
+  }
 }
 
 export default Observer;
