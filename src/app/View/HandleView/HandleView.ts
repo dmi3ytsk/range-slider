0299fbf3c711cfbef76b4d0130d2e2c7e826f0d7
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-01-27 15:39:01 +0200

    update HandleView.ts

--- src/app/View/HandleView/HandleView.ts
+++ src/app/View/HandleView/HandleView.ts
@@ -7,20 +7,33 @@ class HandleView extends Observer {
 
   tip: HTMLElement;
 
-  showTip: boolean = true;
-
+  sliderSize: number;
+  
+  positionOptions: { clientX: number, clientY: number, offsetLeft: number, offsetTop: number, offsetHeight: number};
+  
   constructor(public node: HTMLElement, public options: HandleOptions) {
     super();
 
     this.init();
-  }
+  };
   public getHandle() {
     return this.handle;
-  }
+  };
 
+  public updateOptions(options: HandleOptions) {
+    this.options = options;
+    this.setPosition();
+  };
+
+  public updateSliderSize(size: number) {
+    this.sliderSize = size;
+  };
+  
   private init() {
+    this.setDragListeners
     this.createHandleElements();
-  }
+    this.setPosition();
+  };
 
   private createHandleElements() {
     const tipOn = this.options.showTip ? "" : "_hide";
@@ -33,7 +46,66 @@ class HandleView extends Observer {
     );
 
     this.node.appendChild(this.handle);
-  }
+  };
+
+  private setPosition() {
+    const {isVertical, currentValue, ratio} = this.options;
+    const sliderLength = isVertical? -this.sliderSize :this.sliderSize;
+    const positionVertical = sliderLength * ratio + this.sliderSize;
+    const positionHorizontal = sliderLength * ratio;
+    const position = isVertical ? positionVertical : positionHorizontal;
+
+    if (isVertical) {
+      this.tip.style.top = `${position}px`; ///....???
+    }
+    else {
+      this.tip.style.left = `${position}px`;
+    }
+
+    this.tip.innerText = currentValue.toFixed().toString();
+  };
+
+  private handleWindowMouseUp = () => {
+    window.removeEventListener('mousemove', this.handleWindowMouseMove);
+    window.removeEventListener('mouseup', this.handleWindowMouseUp);
+  };
+
+  private handleWindowMouseMove = (event: MouseEvent) => {
+    const { isVertical } = this.options;
+    const { clientX,
+      clientY,
+      offsetLeft,
+      offsetTop,
+      offsetHeight } = this.positionOptions;
+
+    const position = isVertical
+      ? offsetTop + event.clientY - clientY + offsetHeight / 2
+      : offsetLeft + event.clientX - clientX;
+
+    const value = isVertical
+      ? ((this.sliderSize - position) / this.sliderSize)
+      : (position / this.sliderSize);
+
+    this.broadcast('dragHandle', value);
+  };
+
+  private setDragListeners() {
+    this.handle.addEventListener('mousedown', this.handleRunnerMouseDown);
+  };
+
+  private handleRunnerMouseDown = (event: MouseEvent) => {
+    event.preventDefault();
+    const { target } = event;
+    if (target && target instanceof HTMLElement) {
+      const { clientY, clientX } = event;
+      const { offsetLeft, offsetTop, offsetHeight } = target;
+
+      this.positionOptions = { clientY, clientX, offsetLeft, offsetTop, offsetHeight };
+
+      window.addEventListener('mousemove', this.handleWindowMouseMove);
+      window.addEventListener('mouseup', this.handleWindowMouseUp);
+    };
+  };
 }
 
 export default HandleView;
