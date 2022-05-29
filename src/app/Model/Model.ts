000a4cab34434447079d634cdaf9b6a24d9f8dd8
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-05-29 23:49:45 +0200

    Update Model.ts

--- src/app/Model/Model.ts
+++ src/app/Model/Model.ts
@@ -1,14 +1,40 @@
 import Observer from "../Observer/Observer";
 import defaultAttributes from "../const";
+import { GlobalOptions } from "../interfaces/GlobalOptions";
+import { Ratios } from "../interfaces/Ratios";
 
 class Model extends Observer {
-  data: any;
+  data: GlobalOptions;
+
+  ratios: Ratios;
 
   static defaultAttributes = defaultAttributes;
 
-  constructor(options = Model.defaultAttributes) {
+  constructor(
+    options: GlobalOptions = Model.defaultAttributes,
+  ) {
     super();
     this.data = options;
+    this.calculateNewRatios();
+  }
+
+  public calculateNewRatios() {
+    const { fromCurrentValue, toCurrentValue, min, max } = this.data;
+
+    this.ratios = {
+      fromRatio: (fromCurrentValue - min) / (max - min),
+      toRatio: (toCurrentValue - min) / (max - min)
+    };
+
+    this.broadcast("changeRatios", this.ratios);
+  }
+
+  public getData = (): GlobalOptions => {
+    return this.data;
+  };
+
+  public getRatios(): Ratios {
+    return this.ratios;
   }
 
 }
