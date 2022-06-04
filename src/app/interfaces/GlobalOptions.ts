2ad265aca1bb2a539f49b4a3849f774baf3e24c2
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-06-04 21:05:06 +0200

    Update GlobalOptions.ts

--- src/app/interfaces/GlobalOptions.ts
+++ src/app/interfaces/GlobalOptions.ts
@@ -1,15 +1,14 @@
 export interface GlobalOptions {
-   width?: number,
-   min?: number,
-   max?: number,
-   step?: number,
-   fromCurrentValue?: number,
-   toCurrentValue?: number,
-   isRange?: boolean,
-   isVertical?: boolean,
-   showTip?: boolean,
-   showBar?: boolean,
-   showScale?: boolean,
-   [dataKey: string]: number | boolean;
+  width?: number;
+  min?: number;
+  max?: number;
+  step?: number;
+  fromCurrentValue?: number;
+  toCurrentValue?: number;
+  isRange?: boolean;
+  isVertical?: boolean;
+  showTip?: boolean;
+  showBar?: boolean;
+  showScale?: boolean;
+  [dataKey: string]: number | boolean;
 }
- 
