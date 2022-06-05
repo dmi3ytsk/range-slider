56f20a45de0a3029d7243a9efdf56c06ba2951fc
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-06-05 23:50:53 +0200

    Create DemoPage.ts

---
+++ src/demo-page/DemoPage.ts
@@ -0,0 +1,30 @@
+import "../app/app";
+import SliderConfig from "../blocks/slider-config/SliderConfig";
+import { GlobalOptions } from "../app/interfaces/GlobalOptions";
+
+class DemoPage {
+  $slider: JQuery<Element>;
+
+  sliderConfig: SliderConfig;
+
+  constructor(
+    public root: HTMLElement,
+    public sliderContainer: HTMLElement,
+    options?: GlobalOptions
+  ) {
+    this.init(options);
+  }
+
+  public init(options: GlobalOptions) {
+    const sliderConfig: HTMLFormElement = this.root.querySelector(
+      ".ez-range-slider-config"
+    );
+
+    this.$slider = $(this.root).slider(this.sliderContainer, options);
+    if (sliderConfig) {
+      this.sliderConfig = new SliderConfig(this.$slider, sliderConfig);
+    }
+  }
+}
+
+export default DemoPage;
