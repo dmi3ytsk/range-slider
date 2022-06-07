c63afe29e543f3045d91e0d13520eefed569ef2e
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-06-08 00:03:47 +0200

    BEMification

--- src/demo-page/index.ts
+++ src/demo-page/index.ts
@@ -4,7 +4,7 @@ import DemoPage from "./DemoPage";
 const sliders = document.querySelectorAll(".ez-range-slider");
 
 sliders.forEach((slider: any) => {
-  const sliderContainer = slider.querySelector(".slider-container__slider");
+  const sliderContainer = slider.querySelector(".slider-container__range-slider");
   if (sliderContainer && sliderContainer instanceof HTMLElement) {
     new DemoPage(slider, sliderContainer);
   }
