4781d2fd6514f6e4e2d97e1fa98940b70e822b37
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-06-12 10:29:39 +0200

    add test

---
+++ src/app/View/ScaleView/ScaleView.test.ts
@@ -0,0 +1,99 @@
+import ScaleView from "./ScaleView";
+
+const testOptions = {
+  width: 1000,
+  max: 100,
+  min: 0,
+  step: 10,
+  fromCurrentValue: 50,
+  toCurrentValue: 80,
+  isRange: false,
+  showBar: true,
+  showScale: true,
+  showTip: true,
+  isVertical: false,
+};
+
+let scaleView: ScaleView;
+
+const subscribe = jest.spyOn(ScaleView.prototype, "subscribe");
+const broadcast = jest.spyOn(ScaleView.prototype, "broadcast");
+const init = jest.spyOn(ScaleView.prototype, "init");
+
+describe("Track view", () => {
+  beforeEach(() => {
+    scaleView = new ScaleView(testOptions);
+    subscribe.mockClear();
+    init.mockClear();
+    broadcast.mockClear();
+  });
+
+  describe("Initialization", () => {
+    test("Call all necessary methods", () => {
+      scaleView = new ScaleView(testOptions);
+    });
+
+    test("Set correct data", () => {
+      expect(scaleView.options).toBeInstanceOf(Object);
+      expect(scaleView.options).toEqual(testOptions);
+    });
+
+    test("Can accept all necessary data", () => {
+      scaleView.sliderSize = 500;
+      scaleView.init();
+      expect(scaleView).toBeDefined();
+
+      scaleView.options.isVertical = true;
+      scaleView.init();
+      expect(scaleView).toBeDefined();
+
+      scaleView.options.min = 15500;
+      scaleView.init();
+      expect(scaleView).toBeDefined();
+    });
+  });
+
+  describe("getScale method", () => {
+    test("Set correct data", () => {
+      const scale = scaleView.getScale();
+      expect(scale).toBeInstanceOf(HTMLUListElement);
+      expect(scale).toEqual(scaleView.scale);
+    });
+  });
+
+  describe("reInitScale method", () => {
+    test("Delete all li elements if !showScale", () => {
+      scaleView.options.showScale = false;
+
+      scaleView.reInitScale();
+      expect(scaleView.scale.children.length).toEqual(0);
+    });
+
+    test("Update scale list if scale was init", () => {
+      scaleView.init();
+      scaleView.reInitScale();
+      expect(scaleView.scale).toBeInstanceOf(HTMLUListElement);
+    });
+  });
+
+  describe("reInitScale method", () => {
+    test("Delete all li elements if !showScale", () => {
+      scaleView.options.showScale = false;
+
+      scaleView.reInitScale();
+      expect(scaleView.scale.children.length).toEqual(0);
+    });
+  });
+
+  describe("handleScaleClick method", () => {
+    test("Delete all li elements if !showScale", () => {
+      const newEvent = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      });
+
+      scaleView.handleScaleElementClick(newEvent);
+    });
+  });
+});
---
+++ src/app/View/TrackView/TrackView.test.ts
@@ -0,0 +1,75 @@
+import TrackView from "./TrackView";
+import HandleView from "../HandleView/HandleView";
+
+const testOptions = {
+  fromCurrentValue: 50,
+  toCurrentValue: 80,
+  isRange: false,
+  showBar: true,
+  showScale: true,
+  showTip: true,
+  isVertical: false,
+  ratios: { fromRatio: 0.5, toRatio: 0.8 },
+};
+
+let trackView: TrackView;
+const initTrackElements = jest.spyOn(TrackView.prototype, "initTrackElements");
+const subscribe = jest.spyOn(HandleView.prototype, "subscribe");
+const broadcast = jest.spyOn(HandleView.prototype, "broadcast");
+
+describe("Track view", () => {
+  beforeEach(() => {
+    initTrackElements.mockClear();
+    subscribe.mockClear();
+    broadcast.mockClear();
+    trackView = new TrackView(testOptions);
+  });
+
+  describe("Initialization", () => {
+    test("Call all necessary methods", () => {
+      expect(initTrackElements).toBeCalledTimes(1);
+    });
+
+    test("Set correct data", () => {
+      expect(trackView.options).toEqual(testOptions);
+    });
+  });
+
+  describe("initTrackElements method", () => {
+    test("Create correct Runner number", () => {
+      expect(trackView.handles).toBeInstanceOf(Array);
+
+      expect(trackView.handles).toEqual(
+        expect.arrayContaining([expect.any(HandleView)])
+      );
+
+      trackView.options.isRange = true;
+      trackView.initTrackElements();
+
+      expect(trackView.handles).toEqual(
+        expect.arrayContaining([expect.any(HandleView), expect.any(HandleView)])
+      );
+    });
+
+    test("Subscribe to handle events", () => {
+      expect(subscribe).toBeCalled();
+    });
+  });
+
+  describe("setElementsPosition method", () => {
+    test("Switch bar styles", () => {
+      trackView.setElementsPosition(testOptions);
+      expect(trackView.bar.style.transform).toBeDefined();
+    });
+  });
+
+  describe("updateBar method", () => {
+    test("Update bar when state changes", () => {
+      trackView.updateBar(false);
+      expect(trackView.bar.style.display).toBe("none");
+
+      trackView.updateBar(true);
+      expect(trackView.bar.style.display).toBe("block");
+    });
+  });
+});
