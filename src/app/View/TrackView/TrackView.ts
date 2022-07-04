2d814d5a1c1275d3337d33ed98660e9d11b66353
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-07-05 00:48:22 +0200

    #1 add united tip with from-to value

--- src/app/View/TrackView/TrackView.ts
+++ src/app/View/TrackView/TrackView.ts
@@ -16,7 +16,7 @@ class TrackView extends Observer {
     super();
 
     this.init();
-  };
+  }
 
   public initTrackElements() {
     this.track.innerHTML = "";
@@ -72,15 +72,15 @@ class TrackView extends Observer {
       }
     });
     this.updateBar(showBar);
-  };
+  }
 
   public updateSliderSize(size: number) {
     this.sliderSize = size;
-  };
+  }
 
   public updateOptions(options: TrackOptions) {
     this.options = options;
-  };
+  }
 
   public updateBar(state: boolean) {
     if (state) {
@@ -89,7 +89,7 @@ class TrackView extends Observer {
     } else {
       this.bar.style.display = "none";
     }
-  };
+  }
 
   public setElementsPosition(options: TrackOptions) {
     const {
@@ -124,6 +124,81 @@ class TrackView extends Observer {
       handle.updateSliderSize(this.sliderSize);
       handle.updateOptions(newOptions);
     });
+    let fromOffset: number;
+    let toOffset: number;
+    let tipSize: number;
+    this.handles.forEach((handle, index) => {
+      if (index === 0) {
+        fromOffset = handle.options.isVertical
+          ? handle.tip.offsetTop
+          : handle.tip.offsetLeft;
+      } else {
+        toOffset = handle.options.isVertical
+          ? handle.tip.offsetTop
+          : handle.tip.offsetLeft;
+        tipSize = handle.options.isVertical
+          ? handle.tip.offsetHeight
+          : handle.tip.offsetWidth;
+      }
+    });
+    this.handles.forEach((handle, index) => {
+      if (toOffset - fromOffset < tipSize + 3 && !handle.options.isVertical) {
+        if (index === 0) {
+          if (!Number.isInteger(Number(step))) {
+            const fromCorrectValue: string = fromCurrentValue.toFixed(
+              step
+                .toString()
+                .split("." || ",")
+                .pop().length
+            );
+            const toCorrectValue: string = toCurrentValue.toFixed(
+              step
+                .toString()
+                .split("." || ",")
+                .pop().length
+            );
+            handle.tip.innerHTML = `from ${fromCorrectValue} to ${toCorrectValue}`;
+          } else {
+            handle.tip.innerHTML = `from ${fromCurrentValue} to ${toCurrentValue}`;
+          }
+        } else {
+          handle.tip.style.opacity = "0";
+        }
+      } else if (fromOffset - toOffset < tipSize && handle.options.isVertical) {
+        if (index === 0) {
+          if (!Number.isInteger(Number(step))) {
+            const fromCorrectValue: string = fromCurrentValue.toFixed(
+              step
+                .toString()
+                .split("." || ",")
+                .pop().length
+            );
+            const toCorrectValue: string = toCurrentValue.toFixed(
+              step
+                .toString()
+                .split("." || ",")
+                .pop().length
+            );
+            handle.tip.innerHTML = `${fromCorrectValue} ${toCorrectValue}`;
+          } else {
+            handle.tip.innerHTML = `${fromCurrentValue} ${toCurrentValue}`;
+            handle.tip.style.height = "inherit";
+            handle.tip.style.marginTop =
+              handle.tip.offsetHeight > tipSize ? "-5px" : "0px";
+          }
+        } else {
+          handle.tip.style.opacity = "0";
+        }
+      } else {
+        if (index === 0 && handle.options.isVertical) {
+          handle.tip.style.height = "1.5rem";
+          handle.tip.style.marginTop = "0px";
+        } else {
+          handle.tip.style.opacity = "1";
+        }
+      }
+    });
+
     if (isVertical) {
       this.bar.style.transform = `scaleY(${fromRatio})`;
     } else {
@@ -141,22 +216,22 @@ class TrackView extends Observer {
         }px) scaleY(${toRatio - fromRatio})`;
       }
     }
-  };
+  }
 
   public getTrack() {
     return this.track;
-  };
+  }
 
   private init() {
     this.createTrackElements();
     this.initTrackElements();
-  };
+  }
 
   private createTrackElements() {
     this.track = createElement("div", { className: "range-slider__track" });
 
     this.bar = createElement("div", { className: "range-slider__bar" });
-  };
+  }
 
   private fromHandleChangeListener = (ratio: number) => {
     this.broadcast("dragHandle", { handleNumber: 1, ratio });
@@ -165,6 +240,6 @@ class TrackView extends Observer {
   private toHandleChangeListener = (ratio: number) => {
     this.broadcast("dragHandle", { handleNumber: 2, ratio });
   };
-};
+}
 
 export default TrackView;
